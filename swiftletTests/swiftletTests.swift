036c674eedcc3a35e6b05a4093f330707817fd4c
RayanceKing <88320297+RayanceKing@users.noreply.github.com>
2026-06-16 23:46:42 +0800

    Initial Commit

---
+++ swiftletTests/swiftletTests.swift
@@ -0,0 +1,18 @@
+//
+//  swiftletTests.swift
+//  swiftletTests
+//
+//  Created by 王宇亮 on 6/16/26.
+//
+
+import Testing
+
+struct swiftletTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
